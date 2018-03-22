8f43e7d0bf5d844cd9dd75fe95b411989b00a13e
John <JohnAODowd@users.noreply.github.com>
2018-03-22 08:53:21 +0000

    Add files via upload

--- reader.py
+++ reader.py
@@ -1,12 +1,33 @@
+# Right! Let's start.
+
 from random import choice, shuffle
 
-str = 'T(98)7'
+print("The Great Game")
+
+path = "hand_orderings.txt"
+file = open(path, "r")
+
+def build(file):
+    d = { }
+    for index, line in enumerate(file):
+        perc = round(((index / 16432) * 100) + 1)
+        if perc not in d:
+            d[perc] = []
+        d[perc] += [line.strip()]
+    return d
+
+def random_val(d):
+    values = choice(d)
+    key = None
+    for k in d:
+        if d[k] == values:
+            key = k
+    return key, choice(values)
 
 def format(str, jumble=False):
 	suit_list = ['h', 'd', 'c', 's']
 	out = []
 	suited = False
-
 	for char in str:
 		if char == "(":
 			suit = choice(suit_list)
@@ -20,13 +41,10 @@ def format(str, jumble=False):
 		elif not suited:
 			suit = choice(suit_list)
 			suit_list.remove(suit)
-
 		char += suit
 		out.append(char)
-
 	if jumble:
 		shuffle(out)
-
 	return out
 
 def jumble(card_list):
@@ -38,9 +56,6 @@ def convert_to_png(card_list):
 		card_list[index] = card + ".png"
 	return card_list
 
+str = random_val(build(file))[1]
 print(format(str))
 print(format(str, jumble=True))
-
-
-
-
