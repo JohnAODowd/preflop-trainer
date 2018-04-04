76e9c32b00a4b6089b2f187d942fca1b8637cedc
John <JohnAODowd@users.noreply.github.com>
2018-04-04 19:03:38 +0100

    Update landing.py

--- landing.py
+++ landing.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 import reader
+import jsscript
 
 print('Content-Type: text/html')
 print("")
@@ -23,56 +24,15 @@ print("""
 					display: none;
 				}
 				
-			</style>
-			
-			<script>
-			(function(){
-				var variant_selection;
-				var range_selection;
-				var display_selection;
-
-				document.addEventListener("DOMContentLoaded",init,false);
-
-				function init(){
-					variant_selection = document.querySelector("#variant_selection");
-					range_selection = document.querySelector("#range_selection");
-					display_selection = document.querySelector("#display_selection");
-					variant_selection.addEventListener("submit",handle_variant,false);
-					range_selection.addEventListener("submit",handle_range,false);
-					display_selection.addEventListener("submit",handle_display,false);
-				}
-
-				function handle_variant(event){
-					event.preventDefault();
-					variant_selection.style.display = "none";
-					range_selection.style.display = "initial";
-				}
-				
-				function handle_range(event){
-					event.preventDefault();
-					range_selection.style.display = "none";
-					display_selection.style.display = "initial";
-				}		
-				function handle_display(event){
-					event.preventDefault();
-					var type = document.querySelector('input[name="type"]:checked').value;
-					var range = document.querySelector('input[name="range"]:checked').value;
-					if (range === "Custom"){
-						var custom_val = document.querySelector('#custom_range').value;
-						range = custom_val;
-					}
-					//var call_or_fold = event.originalEvent.explicitOriginalTarget;
-					// grab data from all forms and send to server
-					console.log(type);
-					console.log(range);
-					console.log(event);
-				}	
-			})();	
+			</style> 
+			<script>	
+""")
+print(jsscript.script)
+print("""
 			</script>
         </head>
         <body>
 			<div>
-			
 			
 		    <form id="variant_selection">
 			Select a variant: <br>
@@ -83,7 +43,6 @@ print("""
                 <input type="submit" value="Next">
             </form>
 			
-            
             <form id="range_selection">
 			Select a range: <br>
                 <input type="radio" name="range" value="30"> Open from BTN (30%)         <br>
@@ -92,7 +51,7 @@ print("""
                 <input type="radio" name="range" value="10"> 3-bet out of Position (10%) <br>
                 <input type="radio" name="range" value="Custom">   Custom 
                 <input type="text"  name="custom_range" id="custom_range"  /> % <br>
-                <input type="submit" value="Back"> <input type="submit" value="Next">
+                <input type="submit" value="Next">
             </form>
 			
 			<form id="display_selection">
@@ -102,10 +61,10 @@ for image in reader.image_list :
 				
 print("""
             </p>
-            <p> Percentage: %s </p>
-                  <input type="submit" name="Fold" value="Fold"> <input type="submit" name="Call" id="Call" value="Call">
-            </form>
+            <p> Percentage: <span id="percentage">%s</span> </p>
+                  <input type="button" name="Fold" id="Fold" value="Fold"> <input type="button" name="Call" id="Call" value="Call">
+			</form>
 			
             </div>
         </body>
-    </html> """ % (reader.value[0]))
+    </html> """ % (reader.percentage))
